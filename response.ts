f7cc4c467d1a6d060e52b1fb8cedeb589be9c501
Michael Koeppl <calmandniceperson@protonmail.com>
2020-01-15 15:35:22 +0100

    Indent with 2 spaces instead of 4

--- response.ts
+++ response.ts
@@ -1,13 +1,13 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 
 export class ResponseBuilder {
-    public getResponse(statusCode: number, body: any): APIGatewayProxyResult {
-        return {
-            statusCode: statusCode,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify(body, null, 0)
-        };
-    }
+  public getResponse(statusCode: number, body: any): APIGatewayProxyResult {
+    return {
+      statusCode: statusCode,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(body, null, 0)
+    };
+  }
 }
